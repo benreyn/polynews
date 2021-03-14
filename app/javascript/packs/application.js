58c8ca5a31830f8600bd9d2054e37092a2e6422b
Benjamin Reynolds <breyno127@gmail.com>
2021-03-14 13:43:14 -0500

    Standardize javascript code style with standardjs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,21 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("bootstrap")
-require("jquery")
-require("@fortawesome/fontawesome-free/js/all.js");
-require('./news.js');
+import LocalTime from 'local-time'
 
-import LocalTime from 'local-time';
-LocalTime.start();
+require('@rails/ujs').start()
+require('turbolinks').start()
+require('@rails/activestorage').start()
+require('channels')
+require('bootstrap')
+require('jquery')
+require('@fortawesome/fontawesome-free/js/all.js')
+require('./news.js')
+LocalTime.start()
 
 // stylesheet
-require("../stylesheets/main.scss")
-require("@fortawesome/fontawesome-free/css/all.css")
+require('../stylesheets/main.scss')
+require('@fortawesome/fontawesome-free/css/all.css')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- app/javascript/packs/news.js
+++ app/javascript/packs/news.js
@@ -1,89 +1,82 @@
-'use strict';
+'use strict'
 
 // #############################################################################
 // DOM
 // #############################################################################
 const storyGridElement = () => {
-  return document.querySelector('#story-grid');
-};
+  return document.querySelector('#story-grid')
+}
 
 const pageHasStoryGrid = () => {
-  return !!storyGridElement();
-};
+  return !!storyGridElement()
+}
 
 const pageDataElements = () => {
-  return storyGridElement().querySelectorAll('.story-grid-page-data');
+  return storyGridElement().querySelectorAll('.story-grid-page-data')
 }
 
 const lastPageDataElement = () => {
-  return Array.from(pageDataElements()).pop();
+  return Array.from(pageDataElements()).pop()
 }
 
 const loadingIndicatorElement = () => {
-  return document.querySelector('#story-grid-loading-indicator');
-};
+  return document.querySelector('#story-grid-loading-indicator')
+}
 
 const nextPageUrl = () => {
-  return lastPageDataElement().getAttribute('data-next-page-url');
-};
-
+  return lastPageDataElement().getAttribute('data-next-page-url')
+}
 
 // #############################################################################
 // Predicates
 // #############################################################################
 const lowEnoughToLoadNextPage = () => {
-  const { clientHeight, scrollHeight, scrollTop } = document.documentElement;
+  const { clientHeight, scrollHeight, scrollTop } = document.documentElement
   return scrollTop + clientHeight > scrollHeight - 20
-};
+}
 
 const isLoading = () => {
-  return loadingIndicatorElement().style.display === 'flex';
-};
+  return loadingIndicatorElement().style.display === 'flex'
+}
 
 const shouldLoadNextPage = () => {
-  return lowEnoughToLoadNextPage() && nextPageUrl() && !isLoading();
-};
-
+  return lowEnoughToLoadNextPage() && nextPageUrl() && !isLoading()
+}
 
 // #############################################################################
 // Helpers
 // #############################################################################
 const showLoadingIndicator = () => {
-  loadingIndicatorElement().style.display = 'flex';
-};
-
-const hideLoadingIndicator = () => {
-  loadingIndicatorElement().style.display = 'none';
-};
+  loadingIndicatorElement().style.display = 'flex'
+}
 
 const loadNextPage = () => {
-  showLoadingIndicator();
-  $.getScript(nextPageUrl());
-};
+  showLoadingIndicator()
+  $.getScript(nextPageUrl())
+}
 
 const loadInitialStories = () => {
   if (nextPageUrl() && !isLoading()) {
-    loadNextPage();
+    loadNextPage()
   }
-};
-
+}
 
 // #############################################################################
 // Event Handling
 // #############################################################################
 const handleStoryGridScrolling = () => {
   if (shouldLoadNextPage()) {
-    loadNextPage();
+    loadNextPage()
   }
-};
+}
 
 const handleTurbolinksLoad = () => {
   if (pageHasStoryGrid()) {
-    loadInitialStories();
-    document.addEventListener('scroll', handleStoryGridScrolling);
+    loadInitialStories()
+    document.addEventListener('scroll', handleStoryGridScrolling)
   } else {
-    document.removeEventListener('scroll', handleStoryGridScrolling);
+    document.removeEventListener('scroll', handleStoryGridScrolling)
   }
-};
+}
 
-document.addEventListener('turbolinks:load', handleTurbolinksLoad);
+document.addEventListener('turbolinks:load', handleTurbolinksLoad)
